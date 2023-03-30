314aa8befd4a3cd0e792fadfdaf338d719bfccf8
Javier Puebla <jpuebla1993@gmail.com>
2023-03-30 20:56:00 +0200

    add all CRUD handlers

--- routers/v1/records.py
+++ routers/v1/records.py
@@ -2,37 +2,73 @@
 """
 
 
-from fastapi import APIRouter
+from fastapi import APIRouter, status
+from fastapi.responses import JSONResponse
 
 
 records_router = APIRouter(prefix="/records")
 
 
 @records_router.get("")
-def get_all_records() -> dict:
-    return {}
+def get_all_records() -> JSONResponse:
+    response = [
+        {
+            "id": 1,
+        },
+    ]
+    return JSONResponse(
+        content=response,
+        status_code=status.HTTP_200_OK,
+    )
 
 
 @records_router.post("")
-def create_record(record_id: int) -> dict:
-    return {"record_id": record_id}
+def create_record() -> JSONResponse:
+    response = {
+        "id": 1,
+    }
+    return JSONResponse(
+        content=response,
+        status_code=status.HTTP_201_CREATED,
+    )
 
 
 @records_router.get("/{record_id}")
-def get_record(record_id: int) -> dict:
-    return {"record_id": record_id}
+def get_record(record_id: int) -> JSONResponse:
+    response = {
+        "id": record_id,
+    }
+    return JSONResponse(
+        content=response,
+        status_code=status.HTTP_200_OK,
+    )
 
 
 @records_router.put("/{record_id}")
-def update_record(record_id: int) -> dict:
-    return {"record_id": record_id}
+def update_record(record_id: int) -> JSONResponse:
+    response = {
+        "id": record_id,
+    }
+    return JSONResponse(
+        content=response,
+        status_code=status.HTTP_200_OK,
+    )
 
 
 @records_router.patch("/{record_id}")
-def partial_update_record(record_id: int) -> dict:
-    return {"record_id": record_id}
+def partial_update_record(record_id: int) -> JSONResponse:
+    response = {
+        "id": record_id,
+    }
+    return JSONResponse(
+        content=response,
+        status_code=status.HTTP_200_OK,
+    )
 
 
 @records_router.delete("/{record_id}")
-def delete_record(record_id: int) -> dict:
-    return {"record_id": record_id}
+def delete_record(record_id: int) -> JSONResponse:
+    return JSONResponse(
+        content={},
+        status_code=status.HTTP_204_NO_CONTENT,
+    )
