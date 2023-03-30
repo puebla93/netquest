56b57627accf1460c9feab6743a71ff18768b6ad
Javier Puebla <jpuebla1993@gmail.com>
2023-03-30 20:35:20 +0200

    add all records endpoints

--- routers/v1/records.py
+++ routers/v1/records.py
@@ -6,3 +6,33 @@ from fastapi import APIRouter
 
 
 records_router = APIRouter(prefix="/records")
+
+
+@records_router.get("")
+def get_all_records() -> dict:
+    return {}
+
+
+@records_router.post("")
+def create_record(record_id: int) -> dict:
+    return {"record_id": record_id}
+
+
+@records_router.get("/{record_id}")
+def get_record(record_id: int) -> dict:
+    return {"record_id": record_id}
+
+
+@records_router.put("/{record_id}")
+def update_record(record_id: int) -> dict:
+    return {"record_id": record_id}
+
+
+@records_router.patch("/{record_id}")
+def partial_update_record(record_id: int) -> dict:
+    return {"record_id": record_id}
+
+
+@records_router.delete("/{record_id}")
+def delete_record(record_id: int) -> dict:
+    return {"record_id": record_id}
