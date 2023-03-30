2395ddd98a82dd4190bf0f4029a4f949c477a680
Javier Puebla <jpuebla1993@gmail.com>
2023-03-30 20:34:51 +0200

    add JSONResponse as default response class to api router

--- routers/__init__.py
+++ routers/__init__.py
@@ -2,11 +2,12 @@
 """
 
 from fastapi import APIRouter
+from fastapi.responses import JSONResponse
 
 from routers.v1 import v1_router
 
 
-api_router = APIRouter(prefix="/api")
+api_router = APIRouter(prefix="/api", default_response_class=JSONResponse)
 
 
 api_router.include_router(v1_router)
