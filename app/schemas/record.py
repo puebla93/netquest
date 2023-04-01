e80a38ec31bb7cb2362267c052d1e236fdc69cfb
Javier Puebla <jpuebla1993@gmail.com>
2023-04-01 12:36:12 +0200

    add validator for img property

--- app/schemas/record.py
+++ app/schemas/record.py
@@ -1,9 +1,19 @@
-from pydantic import BaseModel
+from urllib.parse import urlparse
+
+from pydantic import BaseModel, validator
 
 
 class RecordBase(BaseModel):
     title: str
-    img: str | None = None
+    img: str
+
+    @validator("img")
+    def validate_img(cls, value):
+        if value:
+            parsed = urlparse(value)
+            if not all([parsed.scheme, parsed.netloc]):
+                raise ValueError("Invalid img value")
+        return value
 
 
 class RecordCreate(RecordBase):
