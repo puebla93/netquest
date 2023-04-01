2a0172e82b57c5b91d9b8e3e0a45a406f9d5f78d
Javier Puebla <jpuebla1993@gmail.com>
2023-04-01 19:44:34 +0200

    delete commented code

--- app/database.py
+++ app/database.py
@@ -8,9 +8,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from config import settings
 
 
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_SERVER}/{settings.POSTGRES_DB}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
