7662cfeece2032f8135ba3421b99a56e75712d81
Javier Puebla <jpuebla1993@gmail.com>
2023-04-01 12:52:12 +0200

    add RecordUpdate schema to schemas

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,9 +1,10 @@
 from .user import User, UserCreate
-from .record import Record, RecordCreate
+from .record import Record, RecordCreate, RecordUpdate
 
 __all__ = [
     "User",
     "UserCreate",
     "Record",
     "RecordCreate",
+    "RecordUpdate",
 ]
