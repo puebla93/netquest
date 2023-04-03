7726145399964af87f73647c17cd9997715e5261
Javier Puebla <jpuebla1993@gmail.com>
2023-04-03 18:38:50 +0200

    add all created dependecies to dependencies module

--- app/dependencies/__init__.py
+++ app/dependencies/__init__.py
@@ -1,11 +1,16 @@
 """Module to add all dependencies
 """
 
+from .user import get_user
 from .database import get_db
 from .record import get_record
+from .auth import get_current_user, user_is_authenticated
 
 
 __all__ = [
     "get_db",
+    "get_user",
     "get_record",
+    "get_current_user",
+    "user_is_authenticated",
 ]
