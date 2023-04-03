b3f32a094c88516d2515e19111221cbc47065888
Javier Puebla <jpuebla1993@gmail.com>
2023-04-03 18:42:02 +0200

    include auth router to v1 router

--- app/routers/v1/__init__.py
+++ app/routers/v1/__init__.py
@@ -3,10 +3,12 @@
 
 from fastapi import APIRouter
 
+from .auth import auth_router
 from .records import records_router
 
 
 v1_router = APIRouter(prefix="/v1")
 
 
+v1_router.include_router(auth_router)
 v1_router.include_router(records_router)
